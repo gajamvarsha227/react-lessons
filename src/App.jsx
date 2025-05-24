45a87c533ca6f10b38c70f0a67e2a2349a28b716
gajamvarsha227 <23r11a0518@gcet.edu.in>
2025-05-24 20:14:10 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import App1 from "./components/App1. jsx" ;
+import App2 from "./components/App2. jsx";
+import Header from "./components/Header. jsx";
+import Footer from "./components/Footer . jsx";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route index element={<App1 />}></Route>
+        <Route path="/" element={<App1 />}></Route>
+        <Route path="/app2" element={<App2 />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Test from "./components/Test.jsx";
+createRoot(document.getElementById("root")).render(< App/>);
+
+//React creates virtual DOM
+//Physical root
+//Virtual root
+//Reactjs updates virtual root, which automatically updated physical root

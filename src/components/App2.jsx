deb0da6f160bceaad6080d1227f82aa17f543153
gajamvarsha227 <23r11a0518@gcet.edu.in>
2025-05-25 18:11:58 +0530

    update

--- src/components/App2.jsx
+++ src/components/App2.jsx
@@ -9,9 +9,14 @@ export default function App2() {
   return (
     <div>
       <h1>This is App2 component</h1>
+      <h2>Working with Arrays and Objects,Applying Styles</h2>
       <div className="App-Product-Container">
         {products.map((value) => (
-          <Product value={value}  />
+          <div className="App-Product-Box">
+            <h2>{value.name}</h2>
+            <h3>{value.price}</h3>
+            <button>Add to Cart</button>
+          </div>
         ))}
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Test from ". /components/Test.jsx";
+import Test from "./components/Test.jsx";
 createRoot(document.getElementById("root")).render(<App />);
 
 //React creates virtual DOM

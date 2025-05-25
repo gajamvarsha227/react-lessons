f78ecc28a22326c7bfc0579d09de9f1fcad7c175
gajamvarsha227 <23r11a0518@gcet.edu.in>
2025-05-25 17:46:49 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import App1 from "./components/App1.jsx" ;
-import App2 from "./components/App2.jsx";
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
-import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import App1 from "./components/App1";
+import App2 from "./components/App2";
+
+// Use HashRouter for smooth routing on Vercel
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Header />
+
       <Routes>
-        <Route index element={<App1 />}></Route>
-        <Route path="/" element={<App1 />}></Route>
-        <Route path="/app2" element={<App2 />}></Route>
+        <Route path="/" element={<App1 />} />
+        <Route path="/app2" element={<App2 />} />
       </Routes>
+
       <Footer />
-    </BrowserRouter>
+    </Router>
   );
 }
+
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Test from "./components/Test.jsx";
-createRoot(document.getElementById("root")).render(< App/>);
+import Test from ". /components/Test.jsx";
+createRoot(document.getElementById("root")).render(<App />);
 
 //React creates virtual DOM
 //Physical root

6b4e137708b994c5c09d76c4f57a9934f3e679f0
gajamvarsha227 <23r11a0518@gcet.edu.in>
2025-05-26 20:25:32 +0530

    update

--- src/components/App15.jsx
+++ src/components/App15.jsx
@@ -1,21 +1,38 @@
-import React from "react";
-import { useRef } from "react";
+import React, { useRef, useState } from "react";
+
 export default function App15() {
-  const nameRef = useRef();
-  const handleSubmit = () => {
-    alert(nameRef.current.value);
-    nameRef.current.style.color = "blue"
+  const num1Ref = useRef();
+  const num2Ref = useRef();
+  const colorRef = useRef();
+  const resultRef = useRef();
+  const [sum, setSum] = useState(0);
+
+  const handleAdd = () => {
+    const n1 = Number(num1Ref.current.value);
+    const n2 = Number(num2Ref.current.value);
+    setSum(n1 + n2);
   };
+
+  const handleColor = () => {
+    const color = colorRef.current.value;
+    resultRef.current.style.color = color;
+  };
+
   return (
     <div>
       <h1>App15</h1>
       <h2>useRef hook - to create uncontrolled component</h2>
       <p>
-        <input type="text" ref={nameRef} />
+        <input type="text" ref={num1Ref} /> <input type="text" ref={num2Ref} />{" "}
+        <button onClick={handleAdd}>Submit</button>
       </p>
-      <button onClick={handleSubmit}>Submit</button>
+      <p>{sum}</p>
       <hr />
-      
+      <p>
+        <input type="text" ref={colorRef} />{" "}
+        <button onClick={handleColor}>Submit</button>
+      </p>
+      <h3 ref={resultRef}>Hello world</h3>
     </div>
   );
 }
